a421b3a107d1486e0f4f4d32515a1ca520da5b5b
isantiagoa01 <150673491+isantiagoa01@users.noreply.github.com>
2023-11-13 12:25:24 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Script que genera un arbol de directorios 
+REM 
+REM Autor: Ivan santiago Algado 
+REM Fecha: 13-11-2023
+
+mkdir %userprofile%\asir %userprofile%\dam %userprofile%\daw %userprofile%\smr %userprofile%\ceti
+mkdir %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\exam
+mkdir %userprofile%\dam\docs %userprofile%\dam\pract %userprofile%\dam\exam
+mkdir %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\exam
+mkdir %userprofile%\smr\docs %userprofile%\smr\pract %userprofile%\smr\exam
+mkdir %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\exam
