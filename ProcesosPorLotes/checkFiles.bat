b0b4979ec3f01feee0c58f17c1c548d67893a72c
isantiagoa01 <150673491+isantiagoa01@users.noreply.github.com>
2023-11-17 10:57:48 +0100

    Add files via upload

---
+++ ProcesosPorLotes/checkFiles.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Pedir al usuario el nombre de un archivo y comprobar si existe.
+REM autor:Ivan Santiago algado 
+REM
+REM Fecha:17-11-2023
+
+title checkFiles
+
+set /P nombreArchivo="Introduzca el nombre del archivo"
+if exist %nombreArchivo% (echo "Existe el Archivo") else (goto existe)
+:existe
+echo Ya existe un archivo con ese nombre , escriba otro nombre
+set /P nombreArchvo2="Introduzca otro nombre para el archivo"
+echo > %nombreArchivo2%
