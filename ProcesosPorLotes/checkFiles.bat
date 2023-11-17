c2e27bf00af55ddf5e8ef7dde2a373b18f57583b
isantiagoa01 <150673491+isantiagoa01@users.noreply.github.com>
2023-11-17 10:59:42 +0100

    checkFiles.bat

--- ProcesosPorLotes/checkFiles.bat
+++ ProcesosPorLotes/checkFiles.bat
@@ -8,7 +8,9 @@ title checkFiles
 
 set /P nombreArchivo="Introduzca el nombre del archivo"
 if exist %nombreArchivo% (echo "Existe el Archivo") else (goto existe)
+
 :existe
 echo Ya existe un archivo con ese nombre , escriba otro nombre
 set /P nombreArchvo2="Introduzca otro nombre para el archivo"
 echo > %nombreArchivo2%
+exit
